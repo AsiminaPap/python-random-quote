f2c6bdafa338d78d0d98b89b0ec6e2e340ea49d0
AsiminaPap <44819241+AsiminaPap@users.noreply.github.com>
2020-11-20 22:17:56 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,17 @@
+import random
+
 def main():
+  
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  
+  last = 13
+  rnd = random.randint(0, last)
 
-  print(quotes)
+  print(rnd)
 
 if __name__== "__main__":
   main()
