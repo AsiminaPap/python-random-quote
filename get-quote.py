09f978fa14f50991d68529e251035981a5130a9a
AsiminaPap <44819241+AsiminaPap@users.noreply.github.com>
2020-11-20 22:23:23 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -8,13 +8,8 @@ def main():
   quotes = f.readlines()
   f.close()
   
-  last = 13
-  rnd = random.randint(0, last)
-  
-  last = len(quotes) - 1
-  print last
-
-  print([rnd])
+  quotes.append ("The ice is now fractured.")
+  del quotes[-1]
 
 if __name__== "__main__":
   main()
